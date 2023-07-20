9c3fa4ed81b85e0bc02fc569409fd2037a62a33c
Cavaughn Browne <cxbrowne@amazon.com>
2023-07-20 11:05:49 -0500

    allow registry mirror configurations to be mutable for BR

--- controlplane/kubeadm/internal/webhooks/kubeadm_control_plane.go
+++ controlplane/kubeadm/internal/webhooks/kubeadm_control_plane.go
@@ -192,6 +192,7 @@ func (webhook *KubeadmControlPlane) ValidateUpdate(_ context.Context, oldObj, ne
 		{spec, kubeadmConfigSpec, clusterConfiguration, controllerManager, "*"},
 		{spec, kubeadmConfigSpec, clusterConfiguration, scheduler},
 		{spec, kubeadmConfigSpec, clusterConfiguration, scheduler, "*"},
+		{spec, kubeadmConfigSpec, clusterConfiguration, "registryMirror", "*"},
 		// spec.kubeadmConfigSpec.initConfiguration
 		{spec, kubeadmConfigSpec, initConfiguration, nodeRegistration},
 		{spec, kubeadmConfigSpec, initConfiguration, nodeRegistration, "*"},
@@ -217,6 +218,7 @@ func (webhook *KubeadmControlPlane) ValidateUpdate(_ context.Context, oldObj, ne
 		{spec, kubeadmConfigSpec, joinConfiguration, "bottlerocketControl", "*"},
 		{spec, kubeadmConfigSpec, joinConfiguration, "bottlerocketCustomBootstrapContainers"},
 		{spec, kubeadmConfigSpec, joinConfiguration, "bottlerocketSettings", "*"},
+		{spec, kubeadmConfigSpec, joinConfiguration, "registryMirror", "*"},
 		{spec, kubeadmConfigSpec, joinConfiguration, "pause", "*"},
 		// spec.kubeadmConfigSpec
 		{spec, kubeadmConfigSpec, preKubeadmCommands},
